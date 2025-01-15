dbce286013916d7ec14ba6f096254299efdc220d
Karina <20hs.mm@gmail.com>
2025-01-15 10:35:09 +0100

    dots slick slide

--- src/js/script.js
+++ src/js/script.js
@@ -11,13 +11,14 @@
 $(document).ready(function(){
   $('.carousel__inner').slick({
     speed: 1200,
-    adaptiveHeight: true,
+    adaptiveHeight: false,
     prevArrow: '<button type="button" class="slick-prev"><img src="icons/slider/left-arrow.svg"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="icons/slider/right-arrow.svg"></button>',
     responsive: [
         {
             breakpoint: 992,
             settings: {
+                dots: true,
                 arrows: false
             }
         }
