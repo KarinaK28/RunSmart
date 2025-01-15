8737aa216e3a79f08d618ac4b374802606dde0cf
Karina <20hs.mm@gmail.com>
2025-01-15 09:40:04 +0100

    add slick-slider

---
+++ src/js/script.js
@@ -0,0 +1,26 @@
+// $(document).ready(function(){
+//   $(".carousel__inner").owlCarousel({
+//     loop:true,
+//     margin:10,
+//     nav:true
+// }
+// );
+// });
+
+
+$(document).ready(function(){
+  $('.carousel__inner').slick({
+    speed: 1200,
+    adaptiveHeight: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="icons/slider/left-arrow.svg"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="icons/slider/right-arrow.svg"></button>',
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                arrows: false
+            }
+        }
+    ]
+});
+});
